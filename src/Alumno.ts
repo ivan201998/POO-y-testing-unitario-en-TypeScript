7e063a910a6f3cccf38d71c56567c16455ea0815
ivan201998 <ipintos106@gmail.com>
2026-08-19 17:21:24 -0300

    se agrego una herencia persona con persona y profesor

--- src/Alumno.ts
+++ src/Alumno.ts
@@ -1,9 +1,13 @@
-export class Alumno {
+import { Persona } from "./Persona";
+
+export class Alumno extends Persona {
   constructor(
-    public nombre: string,
+    nombre: string,
     public edad: number,
-    public legajo: number
-  ) {}
+    legajo: number
+  ) {
+    super(nombre, legajo);
+  }
 
   esMayorDeEdad(): boolean {
     return this.edad >= 18;
--- src/Profesor.ts
+++ src/Profesor.ts
@@ -1,6 +1,7 @@
-export class Profesor {
-  constructor(
-    public nombre: string,
-    public legajo: number
-  ) {}
+import { Persona } from "./Persona";
+
+export class Profesor extends Persona {
+  constructor(nombre: string, legajo: number) {
+    super(nombre, legajo);
+  }
 }

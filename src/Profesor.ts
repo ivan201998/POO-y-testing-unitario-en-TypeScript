0185b88cf665388728187b09fcfc237f074ea1bb
ivan201998 <ipintos106@gmail.com>
2026-08-19 19:57:56 -0300

    herencia v1.2

--- src/Profesor.ts
+++ src/Profesor.ts
@@ -1,9 +1,14 @@
 import { Persona } from "./Persona";
 
+// Con herencia (lo que tenés)
 export class Profesor extends Persona {
   constructor(nombre: string, legajo: number) {
-    super(nombre, legajo);
-    this.legajo//otra forma de ver
-    this.nombre
+    super(nombre, legajo);// Persona se encarga de asignar
+  
   }
 }
+
+/*//despues mirar como usar esto
+this.legajo//otra forma de ver
+    this.nombre
+*/

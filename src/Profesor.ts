8e9a0abf7ecad5953ea2e10ed418764924acbf6f
ivan201998 <ipintos106@gmail.com>
2026-08-19 19:52:54 -0300

    herencia v1.1

--- src/Profesor.ts
+++ src/Profesor.ts
@@ -3,5 +3,7 @@ import { Persona } from "./Persona";
 export class Profesor extends Persona {
   constructor(nombre: string, legajo: number) {
     super(nombre, legajo);
+    this.legajo//otra forma de ver
+    this.nombre
   }
 }
